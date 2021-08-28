8d5f9d553d6d7d3a7da45ebae1d8dc4a2675c9b4
Savioli Pri <stobbeglaucya@gmail.com>
2021-08-28 13:19:02 -0300

    Adicionar exercicio musica

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Musicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Ninguém me ensinou -Lagum");
+            Console.ResetColor();
+            Console.WriteLine();
+            
+            Console.WriteLine("A vida é boa pacarai");
+            Console.WriteLine("Nem sei por que eu 'to chorando");
+            Console.WriteLine("Eu tenho vinte e poucos anos");
+            Console.WriteLine("E não vou parar aqui");
+            Console.WriteLine();
+
+            Console.WriteLine("Eu sinto falta da minha casa");
+            Console.WriteLine("Minha mãe sente minha falta");
+            Console.WriteLine("Tudo bem, essa é a vida que eu escolhi");
+            Console.WriteLine();
+
+            Console.WriteLine("Desde pequeno falam que eu sou curioso");
+            Console.WriteLine("Quem me viu quem me vê");
+            Console.WriteLine("Fala que eu nunca cresci");
+            Console.WriteLine();
+            
+            Console.WriteLine("Não tenho medo de errar");
+            Console.WriteLine("Só medo de desistir");
+            Console.WriteLine("Mas tenho vinte e poucos");
+            Console.WriteLine("E não vou parar aqui");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.ResetColor();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Mas se garotos não choram alguém diz o que eu sou?");
+            Console.WriteLine("Nanananão, fica calado");
+            Console.WriteLine("Eu sou o que eu quiser");
+            Console.WriteLine("Eu sou o sol da meia noite e a lua cheia de manhã");
+            Console.WriteLine("Mas me visto de mim mesmo da cabeça aos pés");
+            Console.WriteLine();
+
+            Console.WriteLine("Eu tenho vinte e tantos planos pra antes dos 30 anos");
+            Console.WriteLine("Alguém diz pra onde vamos");
+            Console.WriteLine("Tenho pressa de existir");
+            Console.WriteLine();
+
+            Console.WriteLine("Me disseram, calma mano");
+            Console.WriteLine("'Cê tem tempo sobrando");
+            Console.WriteLine("Pra errar, e eu 'to certo que não vou parar aqui");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine();
+
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            Console.WriteLine("Ôôô ninguém me ensinou");
+            Console.WriteLine("Pra onde eu vou, ninguém vai");
+            Console.WriteLine("Ôôô ninguém me ensinou");
+            Console.WriteLine("Pra onde eu vou, ninguém vai");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("Quem te ensinou a sonhar?");
+            Console.WriteLine("");
+
+        }
+    }
+}
